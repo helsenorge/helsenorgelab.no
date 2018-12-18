97548b0bf5fa0adc8e55094d18a5ea79a98db509
Vagrant Default User <vagrant@contrib-jessie.raw>
2018-12-18 16:54:27 +0000

    #7 #9 articles FE work

--- norwegian_ehealth/settings/base.py
+++ norwegian_ehealth/settings/base.py
@@ -631,7 +631,7 @@ PASSWORD_REQUIRED_TEMPLATE = 'patterns/pages/wagtail/password_required.html'
 
 
 # Default size of the pagination used on the front-end.
-DEFAULT_PER_PAGE = 20
+DEFAULT_PER_PAGE = 3
 
 
 # Styleguide
