70a9ea28d5657b98a1e98cce5e214517212950ad
Erin Mullaney <erin.mullaney@torchbox.com>
2018-12-19 10:31:28 -0500

    #7 bump default pagination count back up

--- norwegian_ehealth/settings/base.py
+++ norwegian_ehealth/settings/base.py
@@ -631,7 +631,7 @@ PASSWORD_REQUIRED_TEMPLATE = 'patterns/pages/wagtail/password_required.html'
 
 
 # Default size of the pagination used on the front-end.
-DEFAULT_PER_PAGE = 3  # TODO bump back up to 10
+DEFAULT_PER_PAGE = 10
 
 
 # Styleguide
