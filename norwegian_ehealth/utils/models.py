103f0c84edfaf163b7c51ccb2bc726a4c0334ee1
Erin Mullaney <erin.mullaney@torchbox.com>
2019-01-25 09:37:55 -0500

    remove call-to-action snippet, unneeded

--- norwegian_ehealth/utils/models.py
+++ norwegian_ehealth/utils/models.py
@@ -138,64 +138,6 @@ class ListingFields(models.Model):
     ]
 
 
-@register_snippet
-class CallToActionSnippet(models.Model):
-    title = models.CharField(max_length=255)
-    summary = RichTextField(blank=True, max_length=255)
-    image = models.ForeignKey('images.CustomImage', null=True, blank=True, on_delete=models.SET_NULL, related_name='+')
-
-    link = StreamField(
-        blocks.StreamBlock([
-            (
-                'external_link', blocks.StructBlock([
-                    ('url', blocks.URLBlock()),
-                    ('title', blocks.CharBlock()),
-                ], icon='link')
-            ),
-            (
-                'internal_link', blocks.StructBlock([
-                    ('page', blocks.PageChooserBlock()),
-                    ('title', blocks.CharBlock(required=False)),
-                ], icon='link'),
-            ),
-        ], max_num=1, required=True),
-        blank=True
-    )
-
-    panels = [
-        FieldPanel('title'),
-        FieldPanel('summary'),
-        ImageChooserPanel('image'),
-        StreamFieldPanel('link'),
-    ]
-
-    def get_link_text(self):
-        # Link is required, so we should always have
-        # an element with index 0
-        block = self.link[0]
-
-        title = block.value['title']
-        if block.block_type == 'external_link':
-            return title
-
-        # Title is optional for internal_link
-        # so fallback to page's title, if it's empty
-        return title or block.value['page'].title
-
-    def get_link_url(self):
-        # Link is required, so we should always have
-        # an element with index 0
-        block = self.link[0]
-
-        if block.block_type == 'external_link':
-            return block.value['url']
-
-        return block.value['page'].get_url()
-
-    def __str__(self):
-        return self.title
-
-
 @register_setting
 class SocialMediaSettings(BaseSetting):
     twitter_handle = models.CharField(
