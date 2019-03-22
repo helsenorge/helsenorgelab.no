8c47f9685c9cfe918a80270d4702c58c1dafeb61
Ole Kristian Losvik <losolio@github.user>
2019-03-22 10:57:06 +0100

    Fixed line lengt

--- website/home/models.py
+++ website/home/models.py
@@ -33,7 +33,8 @@ class HomePage(BasePage):
 
     # Only allow creating HomePages at the root level
     parent_page_types = ['wagtailcore.Page']
-    subpage_types = ['news.NewsIndex', 'standardpages.IndexPage', 'standardpages.InformationPage', 'people.PersonIndexPage']
+    subpage_types = ['news.NewsIndex', 'standardpages.IndexPage', 'standardpages.InformationPage',
+                     'people.PersonIndexPage']
 
     hero_title = models.CharField(null=True, blank=False, max_length=80)
 
