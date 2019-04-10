964c354e8a9e868df8e6a5b8bbca374c3c017181
Rich Brennan <rich.brennan@torchbox.com>
2019-04-10 11:55:32 +0100

    add config for referer policy and csp headers

--- website/settings/base.py
+++ website/settings/base.py
@@ -125,6 +125,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
+    'django_referrer_policy.middleware.ReferrerPolicyMiddleware',
+
     'wagtail.core.middleware.SiteMiddleware',
     'wagtail.contrib.redirects.middleware.RedirectMiddleware',
 ]
@@ -550,6 +552,31 @@ if env.get('SECURE_BROWSER_XSS_FILTER', 'true').lower().strip() == 'true':
 # https://docs.djangoproject.com/en/stable/ref/settings/#secure-content-type-nosniff
 if env.get('SECURE_CONTENT_TYPE_NOSNIFF', 'true').lower().strip() == 'true':
     SECURE_CONTENT_TYPE_NOSNIFF = True
+
+
+# Content Security policy settings
+# http://django-csp.readthedocs.io/en/latest/configuration.html
+if 'CSP_DEFAULT_SRC' in env:
+    MIDDLEWARE.append('csp.middleware.CSPMiddleware')
+
+    # The “special” source values of 'self', 'unsafe-inline', 'unsafe-eval', and 'none' must be quoted!
+    # e.g.: CSP_DEFAULT_SRC = "'self'" Without quotes they will not work as intended.
+
+    CSP_DEFAULT_SRC = env.get('CSP_DEFAULT_SRC').split(',')
+    if 'CSP_SCRIPT_SRC' in env:
+        CSP_SCRIPT_SRC = env.get('CSP_SCRIPT_SRC').split(',')
+    if 'CSP_STYLE_SRC' in env:
+        CSP_STYLE_SRC = env.get('CSP_STYLE_SRC').split(',')
+    if 'CSP_IMG_SRC' in env:
+        CSP_IMG_SRC = env.get('CSP_IMG_SRC').split(',')
+    if 'CSP_CONNECT_SRC' in env:
+        CSP_CONNECT_SRC = env.get('CSP_CONNECT_SRC').split(',')
+    if 'CSP_FONT_SRC' in env:
+        CSP_FONT_SRC = env.get('CSP_FONT_SRC').split(',')
+    if 'CSP_BASE_URI' in env:
+        CSP_BASE_URI = env.get('CSP_BASE_URI').split(',')
+    if 'CSP_OBJECT_SRC' in env:
+        CSP_OBJECT_SRC = env.get('CSP_OBJECT_SRC').split(',')
 
 
 # Referrer-policy header settings.
